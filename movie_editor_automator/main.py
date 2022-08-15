8aa7bd9cb3bf098037b339e5cc8c41b77f6e37c5
yakkshit <63894727+saiyakkshit@users.noreply.github.com>
2022-08-16 00:43:14 +0530

    changed

--- movie_editor_automator/main.py
+++ movie_editor_automator/main.py
@@ -1,3 +1,10 @@
-from moviepy.editor import VideoClip, VideoFileClip
+from moviepy.editor import VideoClip, VideoFileClip, concatenate_videoclips
+from numpy import concatenate
 
-clip1 = VideoFileClip("")
+clip1 = VideoFileClip("https://store1.gofile.io/download/direct/67c6fbc6-332b-458c-ad7c-032558c0cb66/s2.mp4").subclip(10, 20)
+clip2 = VideoFileClip("https://store5.gofile.io/download/direct/e540020a-9953-4798-bc5d-7247e1d4fae9/fpv.mp4").subclip(5, 10)
+
+clip3 = VideoFileClip("one.mp4").subclip(5, 10)
+
+combined = concatenate_videoclips([clip1, clip2, clip3])
+combined.write_videofile("c.mp4")
